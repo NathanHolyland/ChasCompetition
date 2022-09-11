4829b3c1a5c469215015c68dc5a5106610cffdaa
Spooghetti420 <kkovachev1947@gmail.com>
2022-09-11 20:22:01 +0100

    add hold button feature

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import time
+import pygame
 from Tetronimo import Tetronimo
 from Grid import *
 from random import *
@@ -23,13 +24,22 @@ scale_vec = [1/19*resolution[0],1/19*resolution[1]]
 grid = Grid([1,1], [10,15], (190,220,240))
 bgcolor = (200, 230, 255)
 running = True
+clock = pygame.time.Clock()
+FPS = 15
 
 #UI elements
-scoreboard = 
+scoreboard = ...
+
+# inputs
+keys = {
+    pygame.K_LEFT: False,
+    pygame.K_RIGHT: False,
+    pygame.K_DOWN: False
+}
 
 # mainloop
 while running:
-    time_start = time.time()
+    time_start = time.perf_counter()
 
     # rendering
     screen.fill(bgcolor)
@@ -38,7 +48,7 @@ while running:
     pygame.display.flip()
 
     # events
-    if timer >= 0.5:
+    if timer >= 1:
         piece.move([0,1], grid)
         timer = 0
     
@@ -50,25 +60,43 @@ while running:
     for line in clears:
         grid.clearRow(line)
 
-    # inputs
+
+    # input handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
         if event.type == pygame.KEYDOWN:
+            for k in (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_DOWN):
+                if event.key == k:
+                    keys[k] = True
+
             if event.key == pygame.K_d:
                 piece.rotate(1, grid)
             if event.key == pygame.K_a:
                 piece.rotate(-1, grid)
-            if event.key == pygame.K_LEFT:
-                piece.move([-1, 0], grid)
-            if event.key == pygame.K_RIGHT:
-                piece.move([1, 0], grid)
-            if event.key == pygame.K_DOWN:
-                piece.move([0,1], grid)
 
-    time_end = time.time()
+        elif event.type == pygame.KEYUP:
+            for k in (pygame.K_d, pygame.K_a, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_DOWN):
+                if event.key == k:
+                    keys[k] = False
+            
+
+    if keys[pygame.K_LEFT]:
+        piece.move([-1, 0], grid)
+    if keys[pygame.K_RIGHT]:
+        piece.move([1, 0], grid)
+    if keys[pygame.K_DOWN]:
+        piece.move([0,1], grid)
+
+    
+
+    clock.tick(FPS)
+    time_end = time.perf_counter()
     time_elapsed = time_end-time_start
     timer+=time_elapsed
     if piece.activeTimer:
         piece.timer += time_elapsed
+
+
 pygame.quit()
