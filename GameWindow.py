960d3ab7a744cb439632be1c34e631068453315e
Nathan Holyland <94959991+NathanHolyland@users.noreply.github.com>
2022-09-12 19:31:02 +0100

    added SFX folder for rachel

---
+++ GameWindow.py
@@ -0,0 +1,79 @@
+import time
+import pygame
+from Tetronimo import Tetronimo
+from Grid import *
+from random import *
+from UI import *
+
+##game is designed for a square window
+
+def randomPiece():
+    options = ["i","l","j","o","s","z","t"]
+    i = randrange(0, 6)
+    pos = [5, 2]
+    if options[i] == "i" or options[i] == "o":
+        pos = [5.5, 1.5]
+    return Tetronimo(options[i], pos)
+
+class GameWindow:
+    def __init__(self, resolution):
+        self.active = False
+
+        self.activePiece = randomPiece()
+        self.timer = 0
+        self.scale_vec = [1/19*resolution[0],1/19*resolution[1]]
+        self.grid = Grid([1,1], [10,15], (190,220,240))
+        self.bgcolor = (200, 230, 255)
+        self.running = True
+
+    def start(self):
+        self.active = True
+
+    def update(self, dt):
+        if not self.active:
+            return
+
+        if self.timer >= 1:
+            self.activePiece.move([0,1], self.grid)
+            self.timer = 0
+    
+        if (self.activePiece.timer >= self.activePiece.timerLimit) or self.activePiece.timerLimit <= 0:
+            self.grid.commitTetronimo(self.activePiece, [1,1])
+            self.activePiece = randomPiece()
+
+        clears = self.grid.clearCheck()
+        for line in clears:
+            self.grid.clearRow(line)
+        
+        self.timer+=dt
+        if self.activePiece.activeTimer:
+            self.activePiece.timer += dt
+
+    def userInput(self, keys):
+        if not self.active:
+            return
+
+        bound_actions = {
+            pygame.K_LEFT: ["move", [-1, 0]],
+            pygame.K_RIGHT: ["move", [1, 0]],
+            pygame.K_DOWN: ["move", [0, 1]],
+            pygame.K_d: ["rotate", 1],
+            pygame.K_a: ["rotate", -1]
+        }
+
+        for key in keys:
+            if keys[key][0]:
+                if bound_actions[key][0] == "move":
+                    self.activePiece.move(bound_actions[key][1], self.grid),
+                elif bound_actions[key][0] == "rotate":
+                    self.activePiece.rotate(bound_actions[key][1], self.grid)
+                if keys[key][1]:
+                    keys[key][0] = False
+    
+    def render(self, screen):
+        if not self.active:
+            return
+
+        screen.fill(self.bgcolor)
+        self.grid.render(screen, self.scale_vec)
+        self.activePiece.render(screen, self.scale_vec)
